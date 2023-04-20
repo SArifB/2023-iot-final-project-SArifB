61b56f9563b9fa3f7554e39287b8b056032a9cf2
Arif Basharyar <saye.arif@gmail.com>
2023-04-20 13:29:57 +0300

    Add charts

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ navBar.innerHTML = `
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" id="pageLink-1" href="./index.html">Main Page</a>
+            <a class="nav-link" id="PageLink-1" href="./index.html">Main Page</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" id="pageLink-2" href="./index.html">Temperature</a>
+            <a class="nav-link" id="PageLink-2" href="./index.html">Temperature</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" id="pageLink-3" href="./index.html">Humidity in</a>
+            <a class="nav-link" id="PageLink-3" href="./index.html">Humidity in</a>
           </li>
         </ul>
       </div>
@@ -41,7 +41,7 @@ const fetchData = async (source) => {
   }
 };
 
-const createMainTable = async () => {
+const createMainPage = async () => {
   const data = await fetchData(
     `https://webapi19sa-1.course.tamk.cloud/v1/weather/limit/50`
   );
@@ -50,7 +50,7 @@ const createMainTable = async () => {
     <table class="table table-hover">
       <thead class="table-light">
         <tr>
-          <th scope="col">Date</th>
+          <th scope="col">Local Date And Time</th>
           <th scope="col">Datatype</th>
           <th scope="col">Data</th>
         </tr>
@@ -78,11 +78,13 @@ const createMainTable = async () => {
   });
 };
 
-const createSecondTable = async (dataType) => {
+const createSecondaryPage = async (dataType) => {
   const data = await fetchData(
     `https://webapi19sa-1.course.tamk.cloud/v1/weather/${dataType}`
   );
   const table = document.getElementById("SideTable");
+  const chart = document.getElementById("ChartTable");
+
   table.innerHTML = `
     <table class="table table-hover">
       <thead class="table-light">
@@ -98,51 +100,83 @@ const createSecondTable = async (dataType) => {
   `;
   const tableBody = document.getElementById("tableBody");
 
-  data.forEach((elem) => {
-    const date = new Date(elem.date_time);
-    const [date1, date2] = [
-      date.toLocaleDateString(),
-      date.toLocaleTimeString(),
-    ];
-    const row = document.createElement("tr");
+  chart.innerHTML = `
+    <canvas id="Forecast"></canvas>
+    `;
 
+  // const temps = data.map((n) => n[datatype]);
+  const date1 = data.map((n) => new Date(n.date_time).toLocaleDateString());
+  const date2 = data.map((n) => new Date(n.date_time).toLocaleTimeString());
+
+  data.forEach((elem, idx) => {
+    const row = document.createElement("tr");
     row.innerHTML = `
-        <td scope="row">${date1}</td>
-        <td>${date2}</td>
+        <td scope="row">${date1[idx]}</td>
+        <td>${date2[idx]}</td>
         <td>${elem[dataType]}</td>
       `;
     tableBody.appendChild(row);
   });
+
+  // let xVals = date2;
+  // let yVals = temps;
+  // let barColors = ["red", "green", "blue", "orange", "brown"];
+
+  const datatype = Object.entries(data[0])[2][0];
+  new Chart("Forecast", {
+    type: "bar",
+    data: {
+      labels: date2,
+      datasets: [
+        {
+          // backgroundColor: barColors,
+          data: data.map((n) => n[datatype]),
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        colors: { enabled: true },
+        legend: { display: false },
+        title: {
+          display: true,
+          text: `20 latest ${dataType.replace("_", " ")} readings`,
+        },
+      },
+    },
+  });
 };
 
-const assignHeader = (title, id) => {
+const assignHeader = (title) => {
   const header = document.getElementById("Header");
   header.innerHTML = title;
 };
 
 const page = sessionStorage.page;
-if (page === "page1" || page === undefined) {
-  assignHeader("Main Page", 1);
-  createMainTable();
+if (page === "page3") {
+  assignHeader("Humidity in");
+  createSecondaryPage("humidity_in");
 } else if (page === "page2") {
-  assignHeader("Temperature", 2);
-  createSecondTable("temperature");
-} else if (page === "page3") {
-  assignHeader("Humidity in", 3);
-  createSecondTable("humidity_in");
+  assignHeader("Temperature");
+  createSecondaryPage("temperature");
+} else {
+  assignHeader("Main Page");
+  createMainPage();
 }
+// if (page === "page1" || page === undefined)
+// <canvas id="weather-forecast"></canvas>
 
-const link1 = document.getElementById("pageLink-1");
+const link1 = document.getElementById("PageLink-1");
 link1.addEventListener("click", () => {
   sessionStorage.setItem("page", "page1");
 });
 
-const link2 = document.getElementById("pageLink-2");
+const link2 = document.getElementById("PageLink-2");
 link2.addEventListener("click", () => {
   sessionStorage.setItem("page", "page2");
 });
 
-const link3 = document.getElementById("pageLink-3");
+const link3 = document.getElementById("PageLink-3");
 link3.addEventListener("click", () => {
   sessionStorage.setItem("page", "page3");
 });
